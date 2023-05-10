a02572f1e61d5525c5b807b2324ae071ad64cdd5
Muhammad Anas Afzaal <anas.afzaal@dc1-arpatechonline.net>
2023-05-10 18:23:20 +0500

    added navbar component, billboard with random movie api, movie list with movie card component and api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,8 @@
-import useCurrentUser from "@/hooks/useCurrentUser";
+// import useCurrentUser from "@/hooks/useCurrentUser";
+import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import Navbar from "@/components/Navbar";
+import useMovieList from "@/hooks/useMovieList";
 import { NextPageContext } from "next"
 import { getSession, signOut } from "next-auth/react"
 
@@ -21,14 +25,19 @@ export async function getServerSideProps(context: NextPageContext) {
 
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
-
-
+  // const { data: user } = useCurrentUser();
+  const { data: movies = [] } = useMovieList() 
+  console.log(movies)
   return (
     <>
-      <h1 className="text-2xl text-green-500">Netflix Clone</h1>
+      <Navbar/>
+      <Billboard/>
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+      </div>
+      {/* <h1 className="text-2xl text-green-500">Netflix Clone</h1>
       <p className="text-white">Logged in as {user?.name} </p>
-      <button className="bg-white " onClick={()=> signOut()}>Logout</button>
+      <button className="bg-white " onClick={()=> signOut()}>Logout</button> */}
     </>
   )
 }
